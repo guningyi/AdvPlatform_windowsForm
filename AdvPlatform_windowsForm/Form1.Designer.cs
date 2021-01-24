abd17b2f84b513f7e5b6753a7810e93742e02849
guningyi <ningyigu@gmail.com>
2021-01-25 00:43:22 +0800

    store it into github.

---
+++ AdvPlatform_windowsForm/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+
+namespace AdvPlatform_windowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Proxy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "url";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(690, 546);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "开始";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(805, 546);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "退出";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 69);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(41, 181);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(220, 69);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "未加载代理列表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "未加载网址列表";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(391, 42);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(512, 482);
+            this.webBrowser1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 581);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private static Dictionary<string, string> dictProxy;
+        private static Dictionary<string,int> dictUnionUrl;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ AdvPlatform_windowsForm/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace AdvPlatform_windowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dictProxy = new Dictionary<string, string>();
+            dictUnionUrl = new Dictionary<string, int>();
+        }
+        private void button1_click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                         System.IO.StreamReader(openFileDialog1.FileName);
+                //MessageBox.Show(sr.ReadToEnd());
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(line);
+                    //加载地址进入Dict
+                    string[] ip_port = line.Split(':');
+                    dictProxy.Add(ip_port[0],ip_port[1]);
+                }
+
+                //显式打开文件的路径
+                string filePath = openFileDialog1.FileName;
+                label1.Text = filePath;
+
+             
+                sr.Close();
+            }
+        }
+
+
+#if false
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //取出配置好的代理与访问地址
+            string proxy = "http://106.75.128.89:80";
+            foreach (KeyValuePair<string, string> kvp in dictProxy)
+            {
+                proxy = "http://" + kvp.Key + ":" + kvp.Value;
+            }
+
+            string url = "http://www.baidu.com";
+            int count = 0;
+            foreach (KeyValuePair<string, int> kvpp in dictUnionUrl)
+            {
+                url = "http://" + kvpp.Key;
+                count = kvpp.Value;
+            }
+
+            //构造代理对象
+            WebProxy proxyObject = new WebProxy(proxy, true);
+            //构造Http请求
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Proxy = proxyObject;
+            req.Timeout = 10000;
+            req.Method = "PUT";
+
+            //TODO:清空cookies
+
+            //构造Http应答接受器
+            HttpWebResponse webResponse = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(webResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string content = reader.ReadToEnd();
+            reader.Close();
+
+            //用输出窗口显式http response的内容
+            MessageBox.Show(content);
+
+        }
+#endif
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //取出配置好的代理与访问地址
+            string proxy = "http://106.75.128.89:80";
+            foreach (KeyValuePair<string, string> kvp in dictProxy)
+            {
+                proxy = "http://" + kvp.Key + ":" + kvp.Value;
+            }
+
+            string url = "http://www.baidu.com";
+            int count = 0;
+            foreach (KeyValuePair<string, int> kvpp in dictUnionUrl)
+            {
+                url = "http://" + kvpp.Key;
+                count = kvpp.Value;
+            }
+
+            webBrowser1.Navigate(url);
+
+            //用输出窗口显式http response的内容
+            //MessageBox.Show(content);
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                         System.IO.StreamReader(openFileDialog1.FileName);
+                //MessageBox.Show(sr.ReadToEnd());
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    listBox2.Items.Add(line);
+                    dictUnionUrl.Add(line, 1);
+                }
+
+                string filePath = openFileDialog1.FileName;
+                label2.Text= filePath ;
+
+                sr.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
